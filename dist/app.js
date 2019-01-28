0043674ec4dd7a80e4aa7eb8d227074ac018c362
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-01-28 14:45:25 +0900

    before deploy

--- dist/app.js
+++ dist/app.js
@@ -10,5 +10,7 @@ var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var app = (0, _express.default)();
+console.log(__dirname);
+app.use("/", _express.default.static(__dirname + "/../../client/build"));
 var _default = app;
 exports.default = _default;
