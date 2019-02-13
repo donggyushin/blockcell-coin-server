d9bce52bedacd55b69a808b2ea80742fe0846e85
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2019-02-13 15:17:14 +0900

    asdasd

--- src/init.js
+++ src/init.js
@@ -3,13 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-let PORT = 3000;
-
-const env = process.env.NODE_ENV || "dev";
-
-if (env !== "dev") {
-  PORT = process.env.PORT;
-}
+let PORT = 8001;
 
 const handleListen = () => {
   console.log(`blockcell coin server is listening on ${PORT}`);
